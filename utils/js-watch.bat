dc9b9603b2d702552ee367441e6520cd55edd6e1
Dave Lampton (https://github.com/DaveLampton) <298615+DaveLampton@users.noreply.github.com>
2018-09-24 01:31:21 -0700

    Removed sourcemaps from the default configuration. Added comment about how to add them back.

--- utils/js-watch.bat
+++ utils/js-watch.bat
@@ -5,6 +5,8 @@ echo   compiling them to JavaScript, sending them through Babel,
 echo   and outputting .js files to the 'webroot\js' directory.
 echo.
 
-coffee --watch --compile --no-header --map --transpile --output webroot\js templates\js
+coffee --watch --compile --no-header --transpile --output webroot\js templates\js
+
+rem  (For sourcemaps, add the --map option)
 
 exit
